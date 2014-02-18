a0b827c304998e7632e1d2b5b256f838c37cce1d
Jun Lee <li.jun4454@gmail.com>
2014-02-18 15:15:55 +0800

    can route start and up load

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router.route);
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/favicon.ico"] = requestHandlers.icon;
+
+server.start(router.route, handle);
