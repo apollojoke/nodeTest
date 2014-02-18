5106291e3c8e74a94c44817fb7b1b86b54966c21
Jun Lee <li.jun4454@gmail.com>
2014-02-18 17:27:47 +0800

    can upload text

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
-handle["/favicon.ico"] = requestHandlers.icon;
 
 server.start(router.route, handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,25 +1,33 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(){
+function start(response, postData){
 	console.log("Request handler 'start' was called.");
-	return "empty";
 
-	exec("find /", function(error, stdout, stderr){
-		content = stdout;
-	});
+	var body = '<html>' +
+		'<head>' +
+		'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+		'</head>' +
+		'<body>' +
+		'<form action="/upload" method="post">' +
+		'<textarea name="text" rows="20" cols="60"></textarea>' +
+		'<input type="submit" value="Submit text" />' +
+		'</form>' +
+		'</body>' +
+		'</html>';
 
-	return content;
+	response.writeHead(200,{"Content-Type":"text/html"});
+	response.write(body);
+	response.end();
+
 }
 
-function upload(){
+function upload(response, postData){
 	console.log("Request handler 'upload' was called.");
-	return "hello upload"
-}
-
-function icon() {
-    return "no icon";
+	response.writeHead(200,{"Content-Type":"text/plain"});
+	response.write("You have sent: " + querystring.parse(postData).text);
+	response.end();
 }
 
 exports.start = start;
 exports.upload = upload;
-exports.icon = icon;
