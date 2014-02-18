2ccf3439d233a76245857364911d699eaebadafd
Jun Lee <li.jun4454@gmail.com>
2014-02-18 16:28:47 +0800

    non-block

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,23 @@
+var exec = require("child_process").exec;
+
 function start(){
 	console.log("Request handler 'start' was called.");
+	return "empty";
+
+	exec("find /", function(error, stdout, stderr){
+		content = stdout;
+	});
+
+	return content;
 }
 
 function upload(){
 	console.log("Request handler 'upload' was called.");
+	return "hello upload"
 }
 
 function icon() {
-
+    return "no icon";
 }
 
 exports.start = start;
